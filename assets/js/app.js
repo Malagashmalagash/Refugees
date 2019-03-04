d530a63462ef5ccae0c9d2813c18dc550cf53eee
DMAG <clement.paquin@gmail.com>
2019-03-04 12:00:57 +0100

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.scss');
+require('bootstrap');
+const $ = require('jquery');
+
+console.log('Test');
